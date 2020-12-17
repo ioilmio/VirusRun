270fbf00f473381298b23e5efd543a621a0c55af
ioilmio <illuminato.salvatore@gmail.com>
2020-12-17 17:44:08 +0100

    first setup; linters; webpack; phaser; working

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import Phaser from 'phaser';
+
+
+const config = {
+  type: Phaser.AUTO,
+  parent: 'phaser-example',
+  width: 800,
+  height: 600,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 200 },
+    },
+  },
+};
+
+
+const game = new Phaser.Game(config);
+
+game();
